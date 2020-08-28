d5f8d4cf9ad87e315de36197e0378c415ba89020
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-08-28 21:50:31 +0300

    first step

---
+++ Social/src/components/Profile.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://wallpapershome.ru/images/pages/pic_h/6472.jpg"/>
+            </div>
+            <div>
+                <img src="https://wallpapershome.ru/images/pages/pic_h/6472.jpg"/>
+            </div>
+            <div>
+                My post
+                <div>New post</div>
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
