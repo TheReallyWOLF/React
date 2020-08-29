ee91f146e56794b059fdee0dd71cdb2f127a1f40
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-08-29 15:44:42 +0300

    style

--- Social/src/components/Navbar.js
+++ Social/src/components/Navbar.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import './Navbar.css'; // Стили для компонента
+import navBarModule from './Navbar.module.css'; // Стили для компонента
 
 const Navbar = () => {
     return (
-        <nav className='nav'>
-            <div className='item'>
+        <nav className={navBarModule.nav}>
+            <div className={navBarModule.item}>
                 <a>Profile</a>
             </div>
-            <div className='item'>
+            <div className={navBarModule.item}>
                 <a>Messages</a>
             </div>
-            <div className='item'>
+            <div className={navBarModule.item}>
                 <a>News</a>
             </div>
-            <div className='item'>
+            <div className={navBarModule.item}>
                 <a>Music</a>
             </div>
-            <div className='item'>
+            <div className={navBarModule.item}>
                 <a>Settings</a>
             </div>
         </nav>
--- Social/src/components/Profile.js
+++ Social/src/components/Profile.js
@@ -1,23 +1,23 @@
 import React from "react";
-import './Profile.css'; // Стили для компонента
+import profileModule from './Profile.module.css'; // Стили для компонента
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={profileModule.content}>
             <div>
-                <img src="https://wallpapershome.ru/images/pages/pic_h/6472.jpg"/>
+                <img className={profileModule.imageProfile} src="https://wallpapershome.ru/images/pages/pic_h/6472.jpg"/>
             </div>
             <div>
-                <img src="https://wallpapershome.ru/images/pages/pic_h/6472.jpg"/>
+                <img className={profileModule.imageProfile} src="https://wallpapershome.ru/images/pages/pic_h/6472.jpg"/>
             </div>
             <div>
                 My post
                 <div>New post</div>
             </div>
-            <div>
+            <div className={profileModule.item}>
                 post 1
             </div>
-            <div>
+            <div className={profileModule.item}>
                 post 2
             </div>
         </div>
