5b0f1ddd273f63c633962b480471f09f5f1a1cc6
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-04 11:22:27 +0300

    to

--- Social/src/components/Navbar/Navbar.js
+++ Social/src/components/Navbar/Navbar.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import navBarModule from './Navbar.module.css'; // Стили для компонента
+import navBarModule from './Navbar.module.css';
+import {NavLink} from "react-router-dom"; // Стили для компонента
 
 const Navbar = () => {
     return (
         <nav className={navBarModule.nav}>
             <div className={navBarModule.item}>
-                <a href="/profile">Profile</a>
+                <NavLink to="/profile">Profile</NavLink>
             </div>
             <div className={navBarModule.item}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs">Messages</NavLink>
             </div>
             <div className={navBarModule.item}>
-                <a href="/news">News</a>
+                <NavLink to="/news">News</NavLink>
             </div>
             <div className={navBarModule.item}>
-                <a href="/music">Music</a>
+                <NavLink to="/music">Music</NavLink>
             </div>
             <div className={navBarModule.item}>
-                <a href="/settings">Settings</a>
+                <NavLink to="/settings">Settings</NavLink>
             </div>
         </nav>
     )
