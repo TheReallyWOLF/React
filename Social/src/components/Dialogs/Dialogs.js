cd59d13f0e2af55fa883ba910c9b876c865d91a1
TheReallyWOLF <wolf-04-654@yandex.ru>
2020-10-04 11:56:18 +0300

    routing

--- Social/src/components/Dialogs/Dialogs.js
+++ Social/src/components/Dialogs/Dialogs.js
@@ -3,8 +3,35 @@ import dialogStyle from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-        <div className={dialogStyle.dialog}>
-            Dialogs
+        <div className={dialogStyle.dialogWrapper}>
+            <div className={dialogStyle.dialogsItems}>
+                <div className={dialogStyle.dialog + ' ' + dialogStyle.active}>
+                    Уасиа
+                </div>
+                <div className={dialogStyle.dialog}>
+                    Фиедиа
+                </div>
+                <div className={dialogStyle.dialog}>
+                    Пашиа
+                </div>
+                <div className={dialogStyle.dialog}>
+                    Крансоле
+                </div>
+                <div className={dialogStyle.dialog}>
+                    Свитаа
+                </div>
+            </div>
+            <div className={dialogStyle.messageWrapper}>
+                <div className={dialogStyle.message}>
+                    Ку!
+                </div>
+                <div className={dialogStyle.message}>
+                    Ку а рри
+                </div>
+                <div className={dialogStyle.message}>
+                    Пшел вон!
+                </div>
+            </div>
         </div>
     )
 }
